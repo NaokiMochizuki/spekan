95eb46653299559e7476529f2a1e5bf32d3c234d
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2024-05-06 14:47:36 +0900

    とりあえずエラー出ないように

--- app/frontend/pages/account/dashboard/Index.vue
+++ app/frontend/pages/account/dashboard/Index.vue
@@ -1,20 +1,23 @@
 <template>
   <div className="horizontalMenucontainer">
-    <!-- <Switcher /> -->
     <div className="page">
       <Header />
+      <Sidebar />
+      <div className="main-content app-content">
+        <div className="container-fluid">
+          テスト
+        </div>
+      </div>
       <Footer />
-      <Backtotop />
     </div>
+    <Backtotop />
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
-//import Switcher from '../../../components/shared/switcher//switcher.vue'
 import Header from '../../../components/shared/header/header.vue'
-//import Sidebar from '../../../components/shared/sidebar/sidebar.vue'
-//import Rightsidebar from '../../../components/shared/rightsidebar/rightsidebar.vue'
+import Sidebar from '../../../components/shared/sidebar/sidebar.vue'
 import Footer from '../../../components/shared/footer/footer.vue'
 import Backtotop from '../../../components/shared/backtotop/backtotop.vue'
 export default {
