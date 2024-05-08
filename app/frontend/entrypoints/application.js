dfed8d2b6688b18338bfdbc5963e7c822563e2ce
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2024-05-08 20:23:24 +0900

    sidebar

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import store from '../store'
 import router from '../router/'
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import simplebar from 'simplebar-vue';
 import Layout from '../layouts/index.vue'
 
 // CSS
@@ -11,11 +12,13 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { library } from "@fortawesome/fontawesome-svg-core";
-import '@fortawesome/fontawesome-free'
-library.add(fas, far, fab)
+import '@fortawesome/fontawesome-free';
+library.add(fas, far, fab);
+import 'simplebar-vue/dist/simplebar.min.css';
 
 // Vueの適応
 const accountPage = createApp(Layout)
+accountPage.component("simplebar", simplebar)
 accountPage.use(store)
 accountPage.use(router)
 accountPage.mount('#app')
