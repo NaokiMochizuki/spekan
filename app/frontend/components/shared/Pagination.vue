b713cfd2c4eff8780d9d6d23a7a9f8952b2ec4c3
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2024-05-20 19:24:22 +0900

    ページネーションコンポーネントの共通化

---
+++ app/frontend/components/shared/Pagination.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="float-end mt-3">
+    <nav aria-label="Page navigation" class="pagination-style-3" v-if="page.total != 1">
+      <ul class="pagination mb-0 flex-wrap">
+        <li class="page-item" :class="{ disabled: page.current == 1 }">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current - 1)">
+            前へ
+          </a>
+        </li>
+        <li class="page-item" v-if="page.current - 2 > 1">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(1)">1</a>
+        </li>
+        <span v-if="page.current - 3 > 1">...</span>
+        <li class="page-item" v-if="page.current - 2 >= 1">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current - 2)">{{page.current - 2}}</a>
+        </li>
+        <li class="page-item" v-if="page.current - 1 >= 1">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current - 1)">{{page.current - 1}}</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link active" href="javascript:void(0);" @click="onPageChange(page.current)">{{page.current}}</a>
+        </li>
+        <li class="page-item" v-if="page.current + 1 <= page.total">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current + 1)">{{page.current + 1}}</a>
+        </li>
+        <li class="page-item" v-if="page.current + 2 <= page.total">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current + 2)">{{page.current + 2}}</a>
+        </li>
+        <span v-if="page.total > page.current + 3">...</span>
+        <li class="page-item" v-if="page.total >= page.current + 3">
+          <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.total)">{{page.total}}</a>
+        </li>
+        <li class="page-item" :class="{ disabled: page.total == page.current }">
+          <a class="page-link text-primary" href="javascript:void(0);" @click="onPageChange(page.current + 1)">
+            次へ
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pagination',
+  props: {
+    page: {
+      type: Object,
+    }
+  },
+  methods: {
+    onPageChange(nextPage){
+      this.$emit('onPageChange', nextPage)
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- app/frontend/pages/client/user/Index.vue
+++ app/frontend/pages/client/user/Index.vue
@@ -72,45 +72,7 @@
               </tbody>
             </table>
           </div>
-          <div class="float-end mt-3">
-            <nav aria-label="Page navigation" class="pagination-style-3" v-if="page.total != 1">
-              <ul class="pagination mb-0 flex-wrap">
-                <li class="page-item" :class="{ disabled: page.current == 1 }">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current - 1)">
-                    前へ
-                  </a>
-                </li>
-                <li class="page-item" v-if="page.current - 2 > 1">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(1)">1</a>
-                </li>
-                <span v-if="page.current - 3 > 1">...</span>
-                <li class="page-item" v-if="page.current - 2 >= 1">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current - 2)">{{page.current - 2}}</a>
-                </li>
-                <li class="page-item" v-if="page.current - 1 >= 1">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current - 1)">{{page.current - 1}}</a>
-                </li>
-                <li class="page-item">
-                  <a class="page-link active" href="javascript:void(0);" @click="onPageChange(page.current)">{{page.current}}</a>
-                </li>
-                <li class="page-item" v-if="page.current + 1 <= page.total">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current + 1)">{{page.current + 1}}</a>
-                </li>
-                <li class="page-item" v-if="page.current + 2 <= page.total">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.current + 2)">{{page.current + 2}}</a>
-                </li>
-                <span v-if="page.total > page.current + 3">...</span>
-                <li class="page-item" v-if="page.total >= page.current + 3">
-                  <a class="page-link" href="javascript:void(0);" @click="onPageChange(page.total)">{{page.total}}</a>
-                </li>
-                <li class="page-item" :class="{ disabled: page.total == page.current }">
-                  <a class="page-link text-primary" href="javascript:void(0);" @click="onPageChange(page.current + 1)">
-                    次へ
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <Pagination :page="page" @onPageChange="onPageChange" />
         </div>
       </div>
     </div>
@@ -122,9 +84,10 @@ import { mapState, mapActions } from 'vuex'
 import axios from 'axios'
 import Pageheader from '../../../components/shared/pageheader/pageheader.vue'
 import SearchForm from '../../../components/client/user/SearchForm.vue'
+import Pagination from '../../../components/shared/Pagination.vue'
 export default {
   name: 'clientUserIndex',
-  components: { Pageheader, SearchForm },
+  components: { Pageheader, SearchForm, Pagination },
   async mounted(){
     await this.fetchUsers()
   },
@@ -167,9 +130,9 @@ export default {
         //TODO: ここで削除処理
       }
     },
-    //TODO: ここからページネーション処理、後で別コンポーネントに切り出し
-    onPageChange(page){
-      this.page.current = page
+    // Paginationからの参照メソッド
+    onPageChange(newPage){
+      this.page.current = newPage
       this.fetchUsers()
     },
     ...mapActions('user', ['setUsers'])
